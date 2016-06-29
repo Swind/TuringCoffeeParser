21b6a058eb7e69ebd61bc3e68300e35772ae6960
yanagiis <yanagiis@gmail.com>
2016-06-29 15:32:11 +0800

    ProcessParameter refactoring

--- web/client/containers/CookbookEditor/cookbookProcessParameter.js
+++ web/client/containers/CookbookEditor/cookbookProcessParameter.js
@@ -3,6 +3,96 @@ import React, { Component } from 'react'
 import Slider from 'material-ui/Slider'
 import TextField from 'material-ui/TextField'
 
+class RangeParameter extends Component {
+
+  onStartChange(_, v) {
+    this.setState({start: v, end: this.state.end})
+  }
+
+  onEndChange(_, v) {
+    this.setState({start: this.state.start, end: v})
+  }
+
+  onDragStop() {
+    const {onChange} = this.props
+    onChange(this.state.start, this.state.end)
+  }
+
+  componentWillMount() {
+    const {start, end} = this.props
+    this.setState({start: start, end: end})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {start, end} = nextProps
+    this.setState({start: start, end: end})
+  }
+
+  render() {
+    const {step, min, max, title} = this.props
+    const {start, end} = this.state
+    return (
+      <li>
+        <span>{title(start, end)}</span>
+        <Slider step={step} min={min} max={max} value={start} onChange={this.onStartChange.bind(this)} onDragStop={this.onDragStop.bind(this)}/>
+        <Slider step={step} min={min} max={max} value={end} onChange={this.onEndChange.bind(this)} onDragStop={this.onDragStop.bind(this)}/>
+      </li>
+    )
+  }
+}
+
+class SlideParameter extends Component {
+
+  onValueChange(_, v) {
+    this.setState({value: v})
+  }
+
+  onDragStop() {
+    const {onChange} = this.props
+    onChange(this.state.value)
+  }
+
+  componentWillMount() {
+    const {value} = this.props
+    this.setState({value: value})
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {value} = nextProps
+    this.setState({value: value})
+  }
+
+  render() {
+    const {step, min, max, title} = this.props
+    const {value} = this.state
+    return (
+      <li>
+        <span>{title(value)}</span>
+        <Slider step={step} min={min} max={max} value={value} onChange={this.onValueChange.bind(this)} onDragStop={this.onDragStop.bind(this)}/>
+      </li>
+    )
+  }
+}
+
+class TextParameter extends Component {
+
+  onValueChange(_, v) {
+    const {onChange} = this.props
+    onChange(v)
+  }
+
+  render() {
+    const {prefix, suffix, type, value, onChange} = this.props
+    return (
+        <li>
+          <span>{prefix}</span>
+          <TextField type={type} value={value} inputStyle={{'textAlign': 'center'}} onChange={this.onValueChange.bind(this)}/>
+          <span>{suffix}</span>
+        </li>
+    )
+  }
+}
+
 class CookbookProcessParameter extends Component {
 
   componentWillMount() {
@@ -16,99 +106,79 @@ class CookbookProcessParameter extends Component {
 
   render() {
     const {onModify} = this.props
-    let high, totalWater, totalTime, radius, temperature
+    let totalWaterParameter, totalTimeParameter, radiusParameter, temperatureParameter, highParameter
 
     const onChange = () => {
       onModify(this.state.params)
     }
 
-    let cloneParams = Object.assign({}, this.state.params)
-    const onHighStartChange = (_, v) => {
-      cloneParams.high.start = v
-      this.setState({params: cloneParams})
-    }
-
-    const onHighEndChange = (_, v) => {
-      cloneParams.high.end = v
-      this.setState({params: cloneParams})
-    }
-
-    const onTotalWaterChange = (_, v) => {
-      cloneParams.total_water = v
-      this.setState({params: cloneParams})
-    }
-
-    const onTotalTimeChange = (_, v) => {
-      cloneParams.total_time = v
-      this.setState({params: cloneParams})
-    }
-
-    const onRadiusStartChange = (_, v) => {
-      cloneParams.radius.start = v
-      this.setState({params: cloneParams})
-    }
-
-    const onRadiusEndChange = (_, v) => {
-      cloneParams.radius.end = v
-      this.setState({params: cloneParams})
-    }
-
-    const onTemperatureChange = (_, v) => {
-      cloneParams.temperature = v
-      this.setState({params: cloneParams})
+    if (this.props.params.high !== undefined) {
+      let {high} = this.props.params
+      let title = (start, end) => `High: from ${start} to ${end} mm`
+      let onHighChange = (start, end) => {
+        let cloneParams = Object.assign({}, this.props.params)
+        cloneParams.high.start = start
+        cloneParams.high.end = end
+        onModify(cloneParams)
+      }
+      highParameter = <RangeParameter title={title} start={high.start} end={high.end} min={0} max={300} step={5} onChange={onHighChange}/>
     }
 
     if (this.state.params.radius !== undefined) {
-      radius =
-        <li>
-          <span>{`Radius: from ${this.state.params.radius.start} to ${this.state.params.radius.end} mm`}</span>
-          <Slider step={5} min={0} max={300} value={this.state.params.radius.start} onChange={onRadiusStartChange} onDragStop={onChange}/>
-          <Slider step={5} min={0} max={300} value={this.state.params.radius.end} onChange={onRadiusEndChange} onDragStop={onChange}/>
-        </li>
-    }
-
-    if (this.state.params.high !== undefined) {
-      high=
-        <li>
-          <span>{`High: from ${this.state.params.high.start} to ${this.state.params.high.end} mm`}</span>
-          <Slider step={5} min={0} max={300} value={this.state.params.high.start} onChange={onHighStartChange} onDragStop={onChange}/>
-          <Slider step={5} min={0} max={300} value={this.state.params.high.end} onChange={onHighEndChange} onDragStop={onChange}/>
-        </li>
+      let {radius} = this.props.params
+      let title = (start, end) => `Radius: from ${start} to ${end} mm`
+      let onradiusChange = (start, end) => {
+        let cloneParams = Object.assign({}, this.props.params)
+        cloneParams.radius.start = start
+        cloneParams.radius.end = end
+        onModify(cloneParams)
+      }
+      radiusParameter = <RangeParameter title={title} start={radius.start} end={radius.end} min={0} max={300} step={5} onChange={onradiusChange}/>
     }
 
     if (this.state.params.total_water !== undefined) {
-      totalWater =
-        <li>
-          <span>{`Total Water: ${this.state.params.total_water} ml`}</span>
-          <Slider step={10} min={0} max={500} value={this.state.params.total_water} onChange={onTotalWaterChange} onDragStop={onChange}/>
-        </li>
-    }
-
-    if (this.state.params.total_time !== undefined) {
-      totalTime =
-        <li>
-          <span>Total time:</span>
-          <TextField type="number" value={this.state.params.total_time} inputStyle={{'textAlign': 'center'}} onChange={onTotalTimeChange}/>
-          <span>seconds</span>
-        </li>
+      let {total_water} = this.props.params
+      let title = (v) => `Total water: ${v} mm`
+      let onTotalWaterChange = (v) => {
+        let cloneParams = Object.assign({}, this.props.params)
+        cloneParams.total_water = v
+        onModify(cloneParams)
+      }
+      totalWaterParameter = <SlideParameter title={title} value={total_water} min={0} max={300} step={5} onChange={onTotalWaterChange}/>
     }
 
     if (this.state.params.temperature !== undefined) {
-      temperature =
-        <li>
-          <span>{`Temperature: ${this.state.params.temperature} °C`}</span>
-          <Slider step={1} min={0} max={100} value={this.state.params.temperature} onChange={onTemperatureChange} onDragStop={onChange}/>
-        </li>
+      let {temperature} = this.props.params
+      let title = (v) => `Temperature: ${v} °C`
+      let onTemperatureChange = (v) => {
+        let cloneParams = Object.assign({}, this.props.params)
+        cloneParams.temperature = v
+        onModify(cloneParams)
+      }
+      temperatureParameter = <SlideParameter title={title} value={temperature} min={0} max={100} step={1} onChange={onTemperatureChange}/>
     }
+
+    if (this.state.params.total_time !== undefined) {
+      let {total_time} = this.props.params
+      let prefix = 'Total time: '
+      let suffix = 'seconds'
+      let onTotalTimeChange = (v) => {
+        let cloneParams = Object.assign({}, this.props.params)
+        cloneParams.total_time= v
+        onModify(cloneParams)
+      }
+      totalTimeParameter = <TextParameter prefix={prefix} suffix={suffix} value={total_time} onChange={onTotalTimeChange}/>
+    }
+
 
     return (
       <div>
         <ul>
-          {radius}
-          {high}
-          {totalWater}
-          {totalTime}
-          {temperature}
+          {radiusParameter}
+          {highParameter}
+          {totalWaterParameter}
+          {temperatureParameter}
+          {totalTimeParameter}
         </ul>
       </div>
     )
