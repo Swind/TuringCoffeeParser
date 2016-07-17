a7af7f02ae9dda6e9d9a396db8a5d11b4da7a3cb
yanagiis <yanagiis@gmail.com>
2016-07-17 16:41:43 +0800

    Add move process

---
+++ libs/processes/move.js
@@ -0,0 +1,51 @@
+const Base = require('./base');
+const Point = Base.Point;
+
+const QUICK_MOVE_F = 5000;
+
+class Move extends Base.Process {
+
+  static get default() {
+    return Object.assign({}, {
+      type: 'process',
+      name: 'move',
+      coordinates: {
+        x: 0,
+        y: 0,
+      },
+      high: {
+        start: 300,
+      },
+    })
+  }
+
+  constructor(params = Move.default) {
+    super(params);
+  }
+
+  get time() {
+    return 0;
+  }
+
+  get water() {
+    return 0;
+  }
+
+  get length() {
+    return 1;
+  }
+
+  get _points() {
+    const x = this.params.coordinates.x;
+    const y = this.params.coordinates.y;
+
+    const points = [];
+
+    // Quick move to the start point.
+    points.push(new Point(x, y, QUICK_MOVE_F));
+
+    return points;
+  }
+}
+
+module.exports = Move;
