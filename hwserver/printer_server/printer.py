b4f2e5850a51241b5f01f6f6500ca92e6772a93e
yanagiis <yanagiis@gmail.com>
2016-09-25 14:54:29 +0800

    [hwserver] Remove lock and AtomicValue in printer.py
    Unused attributes

--- hwserver/printer_server/printer.py
+++ hwserver/printer_server/printer.py
@@ -7,34 +7,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class _AtomicValue(object):
-
-    def __init__(self, initial_value):
-        self.__lock = Lock()
-        self.__value = initial_value
-
-    @property
-    def value(self):
-        self.__lock.acquire()
-        v = self.__value
-        self.__lock.release()
-        return v
-
-    @value.setter
-    def value(self, v):
-        self.__lock.acquire()
-        self.__value = v
-        self.__lock.release()
-
-
 class PrinterController(object):
 
     def __init__(self, hot_driver, cold_driver):
         self._hot_printer = hot_driver
         self._cold_printer = cold_driver
-
-        self._printer_lock = Lock()
-        self.command_counter = _AtomicValue(0)
 
     def connect(self):
         if self._hot_printer.open() is not True:
@@ -57,8 +34,6 @@ class PrinterController(object):
 
     def send_gcodes(self, hot_command=None, cold_command=None):
 
-        self._printer_lock.acquire()
-
         if hot_command is not None:
             self._hot_printer.write(hot_command)
         if cold_command is not None:
@@ -70,5 +45,3 @@ class PrinterController(object):
         if cold_command is not None:
             while 'ok' not in self._cold_printer.readline():
                 continue
-
-        self._printer_lock.release()
