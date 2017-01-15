e4f637f50724ff3297e56727b1d67d51417305ab
yanagiis <yanagiis@gmail.com>
2017-01-15 23:09:44 +0800

    Remove duplicate code

--- libs/src/processes/index.js
+++ libs/src/processes/index.js
@@ -1,32 +1,8 @@
-const Circle = require('./circle');
-const FixedPoint = require('./fixed-point');
-const Spiral = require('./spiral');
-const SpiralTotalWater = require('./spiral-total-water');
-const Wait = require('./wait');
-const Home = require('./home');
-const Move = require('./move');
-const Calibration = require('./calibration');
-const Mix = require('./mix');
-
-var _processes = {
-  "mix": Mix,
-  "circle": Circle,
-  "fixed_point": FixedPoint,
-  "spiral": Spiral,
-  "spiral total water": SpiralTotalWater,
-  "wait": Wait,
-  "home": Home,
-  "move": Move,
-  "calibration": Calibration,
-}
+const { createProcess } = require('./process')
 
 var _processes_pipeline = [
 ]
 
-
-function createProcess(params){
-    return new _processes[params.name](params);
-}
 
 function load(processes_json){
 
