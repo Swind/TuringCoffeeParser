cc93ec4bee4541ba8fbe2c9505ea460e819defab
yanagiis <yanagiis@gmail.com>
2017-07-20 17:40:19 +0800

    remove yarn.lock
    If doesn't remove it, turing-coffee-libs will not install if other
    node_modules has been installed.

--- redesign_web/clean_build.sh
+++ redesign_web/clean_build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Rebuild 'turing-coffee-libs' in the node_modules"
-rm -rf node_modules/turing-coffee-libs
+rm -rf node_modules/turing-coffee-libs yarn.lock
 yarn install
 yarn build
